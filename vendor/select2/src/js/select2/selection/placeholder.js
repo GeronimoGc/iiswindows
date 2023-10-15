8328e4f8b45d580831e73636704fc22c3131a5fc
GeronimoGc <geronimo24gonzalez@gmail.com>
2023-10-14 19:32:06 -0500

    htm por html

--- vendor/select2/src/js/select2/selection/placeholder.js
+++ vendor/select2/src/js/select2/selection/placeholder.js
@@ -1,49 +1,52 @@
-define([
-  '../utils'
-], function (Utils) {
-  function Placeholder (decorated, $element, options) {
-    this.placeholder = this.normalizePlaceholder(options.get('placeholder'));
+define(['../utils'], function (Utils) {
+    function Placeholder(decorated, $element, options) {
+        this.placeholder = this.normalizePlaceholder(options.get('placeholder'))
 
-    decorated.call(this, $element, options);
-  }
-
-  Placeholder.prototype.normalizePlaceholder = function (_, placeholder) {
-    if (typeof placeholder === 'string') {
-      placeholder = {
-        id: '',
-        text: placeholder
-      };
+        decorated.call(this, $element, options)
     }
 
-    return placeholder;
-  };
+    Placeholder.prototype.normalizePlaceholder = function (_, placeholder) {
+        if (typeof placeholder === 'string') {
+            placeholder = {
+                id: '',
+                text: placeholder,
+            }
+        }
 
-  Placeholder.prototype.createPlaceholder = function (decorated, placeholder) {
-    var $placeholder = this.selectionContainer();
-
-    $placeholder.html(this.display(placeholder));
-    $placeholder.addClass('select2-selection__placeholder')
-                .removeClass('select2-selection__choice');
-
-    return $placeholder;
-  };
-
-  Placeholder.prototype.update = function (decorated, data) {
-    var singlePlaceholder = (
-      data.length == 1 && data[0].id != this.placeholder.id
-    );
-    var multipleSelections = data.length > 1;
-
-    if (multipleSelections || singlePlaceholder) {
-      return decorated.call(this, data);
+        return placeholder
     }
 
-    this.clear();
+    Placeholder.prototype.createPlaceholder = function (
+        decorated,
+        placeholder
+    ) {
+        var $placeholder = this.selectionContainer()
 
-    var $placeholder = this.createPlaceholder(this.placeholder);
+        $placeholder.html(this.display(placeholder))
+        $placeholder
+            .addClass('select2-selection__placeholder')
+            .removeClass('select2-selection__choice')
 
-    this.$selection.find('.select2-selection__rendered').append($placeholder);
-  };
+        return $placeholder
+    }
 
-  return Placeholder;
-});
+    Placeholder.prototype.update = function (decorated, data) {
+        var singlePlaceholder =
+            data.length == 1 && data[0].id != this.placeholder.id
+        var multipleSelections = data.length > 1
+
+        if (multipleSelections || singlePlaceholder) {
+            return decorated.call(this, data)
+        }
+
+        this.clear()
+
+        var $placeholder = this.createPlaceholder(this.placeholder)
+
+        this.$selection
+            .find('.select2-selection__rendered')
+            .append($placeholder)
+    }
+
+    return Placeholder
+})
